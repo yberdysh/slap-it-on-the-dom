4b1e60d4c5c8158ba3332af9d16b9d8144e2b653
Jane Costa <yberdysh@gmail.com>
2018-10-12 15:53:11 -0400

    hugo example

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-// let strings = ['h', 'e', 'l', 'l', 'o']
-
-// function join(arr){
-//   let str = '';
-//    arr.forEach(function(x){
-//     str += x
-//   })
-//   return str;
-// }
-
-// join(strings)
-
-
-const people = [
-{name: "Jordan", age: 26},
-{name: "Jackie", age: 22},
-{name: "McKenzie", age: 22}
-]
-
-
-let button = document.querySelector('button')
-button.addEventListener('click', function(event){
-  let jackie = document.getElementById("jackie")
-  people.forEach(function(person) {
-  jackie.innerHTML += `<li> ${person.name} </li>`
-  });
+document.addEventListener("DOMContentLoaded", () => {
+    const hugoDiv = document.querySelector('#hugo')
+    const button = document.querySelector('button')
+    button.addEventListener("click", async () => {
+      hugoDiv.innerHTML = ""
+      // let response = await fetch('http://localhost:3000/students')
+      // let people = await response.json()
+      // console.log("people", people)
+      //   people.forEach(person => {
+      //       hugoDiv.innerHTML += `<li>${person.name}</li>`
+      //   })
+      fetch('http://localhost:3000/students')
+      .then(res => res.json())
+      .then(people => people.forEach(person => {
+            hugoDiv.innerHTML += `<li>${person.name}</li>`
+        }))
+    })
 })
-// let jackie = document.getElementById("jackie")
-//   people.forEach(function(person) {
-//   jackie.innerHTML += `<li> ${person.name} </li>`
-//  });
+
