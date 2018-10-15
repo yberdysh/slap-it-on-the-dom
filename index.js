81b7c82249e132e693a5c459d0435224304b2efb
Jane Costa <yberdysh@gmail.com>
2018-10-15 19:02:38 -0400

    review with steven

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hugoDiv = document.querySelector('#hugo')
-    const button = document.querySelector('button')
-    button.addEventListener("click", async () => {
-      hugoDiv.innerHTML = ""
-      // let response = await fetch('http://localhost:3000/students')
-      // let people = await response.json()
-      // console.log("people", people)
-      //   people.forEach(person => {
-      //       hugoDiv.innerHTML += `<li>${person.name}</li>`
-      //   })
-      fetch('http://localhost:3000/students')
-      .then(res => res.json())
-      .then(people => people.forEach(person => {
-            hugoDiv.innerHTML += `<li>${person.name}</li>`
-        }))
-    })
-})
+let students = [
+    { "name": "Jordan", "age": 26 },
+    { "name": "Jackie", "age": 22 },
+    { "name": "McKenzie", "age": 22 }
+  ]
 
+// document.addEventListener("DOMContentLoaded", () => {
+//  const button = document.getElementsByTagName("button")[0]
+//   const div = document.getElementById('steven')
+//     button.addEventListener("click",function(){
+//       div.innerHTML = "";
+//       // debugger;
+//       // console.log("DIV", div)
+//       // console.log("innerHTML", typeof div.innerHTML)
+//       // console.log("innerHTML", div.innerHTML === "")
+//       students.forEach(function(student){
+//       // debugger;
+//       console.log(student.name)
+//       div.innerHTML += `<li>
+//       ${student.name}
+//     </li>`
+//     // let li = document.createElement("li")
+//     // li.innerText = student.name
+//     // div.append(li)
+//     })
+//   })
+// })
+
+// async await
+document.addEventListener("DOMContentLoaded", () => {
+ const button = document.getElementsByTagName("button")[0]
+  const div = document.getElementById('steven')
+    button.addEventListener("click",function(){
+      div.innerHTML = "";
+      fetch("http://localhost:3000/students")
+      .then(res => res.json())
+      .then(students => {
+      students.forEach(function(student){
+      // debugger;
+      console.log(student.name)
+      div.innerHTML += `<li>
+      ${student.name}
+    </li>`
+    // let li = document.createElement("li")
+    // li.innerText = student.name
+    // div.append(li)
+    })
+
+      })
+      // console.log(stuff)
+  })
+})
